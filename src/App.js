b4e3c76a5da89c108c3ebc67a964f4561a0197fb
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-05-18 22:08:49 +0530

    Controlled and Uncontrolled Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { ControlledForm } from './ControlledForm';
+import { UncontrolledForm } from './UncontrolledForm';
+
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
+  </>
+);
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={() => goToNext({ age: 150 })}>Next</button>
+  </>
+);
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step 3</h1>
+    <p>Congratulations! You qualify for our senior discount</p>
+    <button onClick={() => goToNext({})}>Next</button>
+  </>
+);
+const StepFour = ({ goToNext }) => (
+  <>
+    <h1>Step 4</h1>
+    <button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
+  </>
+);
 
 function App() {
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onNext = stepData => {
+    setOnboardingData({ ...onboardingData, ...stepData });
+    setCurrentIndex(currentIndex + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <>
+    //   <UncontrolledForm />
+    //   <ControlledForm />
+    // </>
+    // <UncontrolledOnboardingFlow onFinish={data => {
+    //   console.log(data);
+    //   alert('Onboarding complete!');
+    // }}>
+    //   <StepOne />
+    //   <StepTwo />
+    //   <StepThree />
+    // </UncontrolledOnboardingFlow>
+    <ControlledOnboardingFlow
+      currentIndex={currentIndex}
+      onNext={onNext}
+    >
+      <StepOne />
+      <StepTwo />
+      {onboardingData.age >= 62 && <StepThree />}
+      <StepFour />
+    </ControlledOnboardingFlow>
+
   );
 }
 
