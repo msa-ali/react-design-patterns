fbdbd0c6bce8dc30991723165d6635716edb1524
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-05-19 06:12:23 +0530

    hocs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { printProps } from './printProps';
+import { UserInfo } from './UserInfo';
+import {withUser} from './withUser';
+
+const UserInfoWrapped = printProps(UserInfo);
+
+const UserInfoWithLoader = withUser(UserInfo, '234');
+
 function App() {
-  return (
-    <></>
-  );
+	return (
+		// <UserInfoWrapped a={1} b="Hello" c={{ name: 'Shaun' }} />
+
+    <UserInfoWithLoader />
+	);
 }
 
 export default App;
