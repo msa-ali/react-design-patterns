ac3478a90ac79e401881edac69818016a97e67c3
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-05-19 06:39:26 +0530

    fp-in-react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { BigSuccessButton, DangerButton } from "./composition";
+import { RecursiveComponent } from "./RecursiveComponent";
+
+const nestedObject = {
+	a: 1,
+	b: {
+		b1: 4,
+		b2: {
+			b23: 'Hello',
+		},
+		b3: {
+			b31: {
+				message: 'Hi',
+			},
+			b32: {
+				message: 'Hi',
+			}
+		}
+	},
+	c: {
+		c1: 2,
+		c2: 3,
+	}
+}
+
+
+
 function App() {
   return (
-    <></>
+    <>
+      <RecursiveComponent data={nestedObject} />
+      <DangerButton text="Don't do this" />
+      <BigSuccessButton text="Yes!!" />
+    </>
   );
 }
 
